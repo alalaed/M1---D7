67f54f1f40096b9ebcaa91cc1a9d16286dd1a9cf
Ali Alaeddine <a.alaeddine@live.com>
2021-12-07 16:29:43 +0100

    ex 7 - 8

---
+++ homework.js
@@ -0,0 +1,13 @@
+
+
+
+const h1Changer = function(){
+    alert("it works")
+    let heading = document.getElementsByTagName("h1");
+    heading.innerText = "vibez"      
+}
+
+const backgroundChanger = function(){
+    let background = document.getElementsByTagName("body")
+    background.style.background-color = "aqua"
+}
